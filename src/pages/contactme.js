b11cedf292f0777f597f4442e6d570fcd408510f
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-10-19 17:17:31 -0500

    bio work linked in and github links added

--- src/pages/contactme.js
+++ src/pages/contactme.js
@@ -4,13 +4,20 @@ function Contactme () {
     
     <div className="contact-info" id="contact-me">
       <div>
-        <h3>Kyle Sorensen</h3>
+        <h3>Kyle Sorensen<br/>
+          Full Stack Web Developer</h3>
       </div>
 
       <address>
+        <div>
+        <a href="https://linkedin.com"><span className="LinkedIn">LinkedIn</span></a>
+        </div>
+        <div>
+        <a href="https://github.com/ksore85"><span className="LinkedIn">GitHub</span></a>
+        </div>
         Madison, WI <br/>
         P: 608.320.1624
-        E: <a href="mailto:ksore85@gmail.com">ksore85@gmail.com</a>
+        E: <a href="mailto:ksore85@gmail.com">ksore85@gmail.com</a> 
       </address>
     </div>
     
--- src/pages/work.js
+++ src/pages/work.js
@@ -4,14 +4,14 @@ function Work () {
     <main id="work">
         <h2 className="section-title">Work</h2>
         <div className="flex-container">
-          <a className="flex-item first" href="https://ksore85.github.io/code-quiz/" target="_blank">
-            <span>Code Quiz</span>
+          <a className="flex-item first" id="wi-state-parks" href="https://mews0.github.io/wi-state-parks/" target="_blank">
+            <span>WI State Parks</span>
           </a>
-          <div className="flex-item" id="wi-state-parks">
-            <a href="https://mews0.github.io/wi-state-parks/"><span className="title">WI State Parks</span></a>
+          <div className="flex-item" id="code-quiz">
+            <a href="https://ksore85.github.io/code-quiz/"><span className="title">Code quiz</span></a>
           </div>
-          <div className="flex-item" id="">
-            <a href="https://rocky-dusk-49928.herokuapp.com/"><span className="title">Chef's Kiss</span></a>
+          <div className="flex-item" id="work-day-scheduler">
+            <a href="https://ksore85.github.io/work-day-scheduler/"><span className="title">Work Day Scheduler</span></a>
           </div>
           <div className="flex-item" id="chefs-kiss">
             <a href="https://rocky-dusk-49928.herokuapp.com/"><span className="title">Chef's Kiss Food Blog</span></a>
@@ -23,7 +23,7 @@ function Work () {
         
         </div>
     </main>
-    
+    https://ksore85.github.io/code-quiz/
         </>
     )
     }
