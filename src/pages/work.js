26b27f4f8e315d313737b3967325a449d764dd3f
ksore85 <84486015+ksore85@users.noreply.github.com>
2021-10-19 16:18:00 -0500

    change work page/update images

--- src/pages/work.js
+++ src/pages/work.js
@@ -4,23 +4,21 @@ function Work () {
     <main id="work">
         <h2 className="section-title">Work</h2>
         <div className="flex-container">
-          <a className="flex-item first" href="https://ksore85.github.io/run-buddy/" target="_blank">
-            <span>Surf Report</span>
+          <a className="flex-item first" href="https://ksore85.github.io/code-quiz/" target="_blank">
+            <span>Code Quiz</span>
           </a>
-          <div className="flex-item">
-            <span>LED Wall</span>
+          <div className="flex-item" id="wi-state-parks">
+            <a href="https://mews0.github.io/wi-state-parks/"><span className="title">WI State Parks</span></a>
           </div>
-          <div className="flex-item">
-            <span>Calculator</span>
+          <div className="flex-item" id="">
+            <a href="https://rocky-dusk-49928.herokuapp.com/"><span className="title">Chef's Kiss</span></a>
           </div>
           <div className="flex-item" id="chefs-kiss">
-            <a href="https://rocky-dusk-49928.herokuapp.com/"><span className="title">Chef's Kiss</span></a>
+            <a href="https://rocky-dusk-49928.herokuapp.com/"><span className="title">Chef's Kiss Food Blog</span></a>
           </div>
          
             <div className="flex-item" id="run-buddy">
               <a href="https://ksore85.github.io/run-buddy/"><span className="title">Run Buddy</span></a>
-              
-            
             </div>
         
         </div>
